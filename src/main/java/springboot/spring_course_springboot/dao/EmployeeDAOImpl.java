af7b9bead2bbcb262dde7c8d5df45ee9f2395df8
KinlyDog <kinlydog@gmail.com>
2023-07-08 18:02:39 +1000

    Add JPA in project

--- src/main/java/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
+++ src/main/java/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
@@ -1,9 +1,7 @@
 package springboot.spring_course_springboot.dao;
 
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import springboot.spring_course_springboot.entity.Employee;
@@ -17,29 +15,24 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     private EntityManager entityManager;
 
     public List<Employee> getAllEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-
-        return session.createQuery("from Employee", Employee.class).getResultList();
+        return entityManager.createQuery(
+                "from Employee", Employee.class).getResultList();
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-
-        session.saveOrUpdate(employee);
+        Employee newEmp = entityManager.merge(employee);
+        employee.setId(newEmp.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-
-        return session.get(Employee.class, id);
+        return entityManager.find(Employee.class, id);
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Employee> query = session.createQuery
+        Query query = entityManager.createQuery
                 ("delete from Employee where id =:employeeId");
         query.setParameter("employeeId", id);
         query.executeUpdate();
